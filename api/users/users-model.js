8428b3d9d2ecaca5243d3e2b91c709ddd04ba70b
Shanils857 <shanils857@gmail.com>
2024-11-18 15:23:25 -0500

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -7,19 +7,10 @@ function find() {
 }
 
 function findBy(filter) {
-  /**
-    You will need to join two tables.
-    Resolves to an ARRAY with all users that match the filter condition.
-
-    [
-      {
-        "user_id": 1,
-        "username": "bob",
-        "password": "$2a$10$dFwWjD8hi8K2I9/Y65MWi.WU0qn9eAVaiBoRSShTvuJVGw8XpsCiq",
-        "role_name": "admin",
-      }
-    ]
-   */
+  return db('users')
+  ,join('roles', 'users.role_id', 'roles_role_id')
+  .select('user_id', 'username', 'password', 'role_name')
+  .where(filter)
 }
 
 function findById(user_id) {
